7e6e80050629af024a1abfa876e99b923c47996e
NF RIDOY <nfridoy@gmail.com>
2023-12-11 02:54:30 +0600

    tailwind dark fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
+import {
+  // createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import router from "./router/router.jsx";
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx's
 import './index.css'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>,
 )
