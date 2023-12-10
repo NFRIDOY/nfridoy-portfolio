73217df00bffd8d98d8619f4cc0a4e4a683b97f3
NF RIDOY <nfridoy@gmail.com>
2023-12-11 04:49:38 +0600

    Deploy

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Container from "../components/Containers/Container";
 import Navbar from "../components/Navbar/Navbar";
 
@@ -7,7 +8,9 @@ export default function MainLayout() {
         <div>
             <Container>
                 <Navbar></Navbar>
+                <Outlet></Outlet>
             </Container>
+
         </div>
     )
 }
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,12 +3,35 @@ import {
 } from "react-router-dom";
 
 import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
+import Projects from "../pages/Projects/Projects";
+import Skills from "../pages/Skills/Skills";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>,
+            },
+            {
+                path: "/about",
+                element: <About></About>,
+            },
+            {
+                path: "/projects",
+                element: <Projects></Projects>,
+            },
+            {
+                path: "/skills",
+                element: <Skills></Skills>,
+            },
+        ],
     },
+
 ]);
 
 export default router;
