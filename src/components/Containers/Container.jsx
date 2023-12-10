930566700bf00a1fe80cf1c19aef2e1d94e24f82
NF RIDOY <nfridoy@gmail.com>
2023-12-11 03:29:43 +0600

    Navber

---
+++ src/components/Containers/Container.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Container({children}) {
+    return (
+        <div className="max-w-6xl mx-auto mb-4">
+            {children}
+        </div>
+    )
+}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,173 @@
+import { Link, NavLink } from "react-router-dom";
 
 
 export default function Navbar() {
     return (
-        <div>Navbar</div>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+
+                        <li>
+                            <NavLink
+                                to="/about"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "pending" : isActive ? "active" : ""
+                                }
+                            >
+                                About
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/blogs"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "pending" : isActive ? "active" : ""
+                                }
+                            >
+                                Blogs
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/projects"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "pending" : isActive ? "active" : ""
+                                }
+                            >
+                                Projects
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/skills"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "pending" : isActive ? "active" : ""
+                                }
+                            >
+                                Skills
+                            </NavLink>
+                        </li>
+                        
+                        <li>
+                            <button
+                                to="/blogs"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "pending" : isActive ? "active" : ""
+                                }
+                            >
+                                Contact Me
+                            </button>
+                        </li>
+                        <li>
+                            <button
+                                to="/blogs"
+                                className={({ isActive, isPending }) =>
+                                    isPending ? "pending" : isActive ? "active" : ""
+                                }
+                            >
+                                Download My Resume
+                            </button>
+                        </li>
+                        {/* <li>
+                        <details>
+                            <summary>Blogs</summary>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li><a>Item 3</a></li> */}
+
+                    </ul>
+                </div>
+                <a className="font-black  font-sans  text-xl ">
+                    <span className="text-6xl text-neutral italic ">NF</span>
+                    <span className="text-xl text-neutral ">RIDOY</span>
+                </a>
+            </div>
+            <div className="navbar-end hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li>
+                        <NavLink
+                            to="/about"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""
+                            }
+                        >
+                            About
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/blogs"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""
+                            }
+                        >
+                            Blogs
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/projects"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""
+                            }
+                        >
+                            Projects
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/skills"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""
+                            }
+                        >
+                            Skills
+                        </NavLink>
+                    </li>
+
+                    <li>
+                        <button
+                            to="/blogs"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""
+                            }
+                        >
+                            Contact Me
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            to="/blogs"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""
+                            }
+                        >
+                            Download My Resume
+                        </button>
+                    </li>
+                    {/* <li>
+                        <details>
+                            <summary>Blogs</summary>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li><a>Item 3</a></li> */}
+                </ul>
+            </div>
+            {/* <div className="navbar-end">
+                <a className="btn">Button</a>
+            </div> */}
+        </div>
     )
 }
