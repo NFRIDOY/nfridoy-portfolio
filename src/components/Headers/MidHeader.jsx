2dfb4dc975befb4b854a40c8cf2986e61e3550ce
NF RIDOY <nfridoy@gmail.com>
2023-12-27 19:01:08 +0600

    Upadate : skills UI

---
+++ src/components/Headers/MidHeader.jsx
@@ -0,0 +1,9 @@
+
+
+export default function MidHeader({children}) {
+    return (
+        <div className="text-3xl font-bold my-6 text-center">
+            {children}
+        </div>
+    )
+}
--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,4 +1,5 @@
 import Container from "../../components/Containers/Container";
+import MidHeader from "../../components/Headers/MidHeader";
 import SubHeader from "../../components/Headers/SubHeader";
 import Paragraph1 from "../../components/Paragraphs/Paragraph1";
 import MainHeader from './../../components/Headers/MainHeader';
@@ -14,10 +15,136 @@ export default function Skills() {
             <MainHeader>
                 Skills
             </MainHeader>
+            {/* <Container>
+                <section className="flex w-full">
+                    <div className="w-1/2">
+                        <MidHeader>
+                            Front End
+                        </MidHeader>
+                        <section id="frontEnd" className="h-fit  rounded-md p-4 grid grid-cols-2 gap-3">
+                            <div>
+                                <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+                                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40" />
+                                </a>
+                                <p>
+                                    React Js
+                                </p>
+                            </div>
+                            <div>
+                                <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer">
+                                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40" />
+                                </a>
+                                <p>
+                                    JavaScript
+                                </p>
+                            </div>
+                            <div>
+                                <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+                                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40" />
+                                </a>
+                                <p>CSS</p>
+                            </div>
+                            <div>
+                                <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
+                                    <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwind" width="40" height="40" />
+                                </a>
+                                <p>Tailwindcss</p>
+                            </div>
+                            <div>
+                                <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
+                                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40" />
+                                </a>
+                                <p>HTML</p>
+                            </div>
+
+                            <div>
+                                <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+                                    <img src="https://cdn.worldvectorlogo.com/logos/nextjs-2.svg" alt="nextjs" width="40" height="40" />
+                                </a>
+                                <p>Nextjs</p>
+                            </div>
+
+                            <div>
+                                <a href="https://getbootstrap.com" target="_blank" rel="noreferrer">
+                                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40" height="40" />
+                                </a>
+                                <p>Bootstrap</p>
+                            </div>
+
+                            <div>
+                                <a href="https://www.chartjs.org" target="_blank" rel="noreferrer">
+                                    <img src="https://www.chartjs.org/media/logo-title.svg" alt="chartjs" width="40" height="40" />
+                                </a>
+                                <p>Chartjs</p>
+                            </div>
+                        </section>
+                    </div>
+                    <div className="w-1/2">
+                        <div className="">
+                            <MidHeader>
+                                Back End
+                            </MidHeader>
+                            <section id="backEnd" className="h-fit rounded-md p-4 grid grid-cols-2 gap-3">
+                                <div>
+                                    <a href="https://nodejs.org" target="_blank" rel="noreferrer">
+                                        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40" />
+                                    </a>
+                                    <p>Node Js</p>
+                                </div>
+
+                                <div>
+                                    <a href="https://expressjs.com" target="_blank" rel="noreferrer">
+                                        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40" />
+                                    </a>
+                                    <p>Express Js</p>
+                                </div>
+                            </section>
+                        </div>
+                        <div className="">
+                            <MidHeader>
+                                Database
+                            </MidHeader>
+                            <section id="backEnd" className="h-fit rounded-md p-4 grid grid-cols-2 gap-3">
+                                <div>
+                                    <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
+                                        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40" />
+                                    </a>
+                                    <p>
+                                        MongoDB
+                                    </p>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+
+                </section>
+                <section>
+                    <div className="w-1/2">
+                        <MidHeader>
+                            Version Control
+                        </MidHeader>
+                        <section id="frontEnd" className="h-fit  rounded-md p-4 grid grid-cols-2 gap-3">
+                            <div>
+                                <a href="https://git-scm.com/" target="_blank" rel="noreferrer">
+                                    <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40" />
+                                </a>
+                                <p>Git</p>
+                            </div>
+                            <div>
+                                <a href="https://github.com" target="_blank" rel="noreferrer">
+                                    <img src="https://www.vectorlogo.zone/logos/github/github-icon.svg" alt="git" width="40" height="40" />
+                                </a>
+                                <p>GitHub</p>
+                            </div>
+                        </section>
+                    </div>
+                    
+                </section>
+            </Container> */}
 
             <Container>
                 <Paragraph1>
-                    <p id="language" className="grid grid-cols-6 gap-2  p-4">
+                    <p id="language" className="grid grid-cols-4 gap-10  p-4">
                         {/* <div className="skilltype"> */}
                         <div>
                             <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
@@ -75,6 +202,8 @@ export default function Skills() {
                             <p>Chartjs</p>
                         </div>
                         {/* </div> */}
+                        {/* <div> */}
+
 
                         <div>
                             <a href="https://www.figma.com/" target="_blank" rel="noreferrer">
@@ -97,6 +226,11 @@ export default function Skills() {
                             <p>Express Js</p>
                         </div>
 
+                        {/* </div> */}
+                        {/* <div> */}
+
+
+
                         <div>
                             <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
                                 <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40" />
@@ -106,6 +240,7 @@ export default function Skills() {
                             </p>
                         </div>
 
+                        {/* </div> */}
                         <div>
                             <a href="https://firebase.google.com/" target="_blank" rel="noreferrer">
                                 <img src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" alt="firebase" width="40" height="40" />
